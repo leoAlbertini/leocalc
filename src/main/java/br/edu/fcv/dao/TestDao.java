bd0b097038ff93232ca3e42c65ccdcd7c60188c4
leonardoalbertini <leonardo.xvl@gmail.com>
2016-10-22 11:28:17 -0200

    insclusao da modelo, dao e serviço rest modelo

---
+++ src/main/java/br/edu/fcv/dao/TestDao.java
@@ -0,0 +1,20 @@
+package br.edu.fcv.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.edu.fcv.model.Lembrete;
+
+public class TestDao {
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("config-entidades");
+		EntityManager em = factory.createEntityManager();
+		Lembrete lembrete = new Lembrete();
+		lembrete.setLembrete("aula sabado de manha");
+		em.getTransaction().begin();
+		em.persist(lembrete);
+		em.getTransaction().commit();
+	}
+
+}
